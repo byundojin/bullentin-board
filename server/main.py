3241d8f0aaa90f4b71edff36d614aa84793170de
byundojin <byundojin0216@gmail.com>
2024-09-18 00:43:23 +0900

    post-post

--- server/main.py
+++ server/main.py
@@ -24,3 +24,30 @@ try:
     make_table()
 except sqlite3.OperationalError as e:
     assert(e.args[0] == 'table posts already exists')
+
+def create_post(title, content):
+    post_create_sql = \
+"""
+insert into posts(title, content) values (?, ?);
+"""
+
+    cursor = db_connection.cursor()
+    cursor.execute(post_create_sql, (title, content))
+    db_connection.commit()
+
+class PostBody(BaseModel):
+    title:str
+    content:str
+
+@app.post("/post")
+async def post_post(post_body:PostBody = Body()):
+    create_post(
+        title=post_body.title,
+        content=post_body.content
+    )
+
+    return Response(status_code=200)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app)
